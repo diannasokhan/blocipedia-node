59f7001c7fdcaa09dd34b8c31f70f171c44ff009
Dianna Sokhan <diannasokhan@gmail.com>
2019-09-05 16:14:39 -0400

    scaffolding

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const http = require("http");
 const port = normalizePort(process.env.PORT || "3000");
  app.set("port", port);
 
+ const server = http.createServer(app);
+
  server.listen(port);
 
  function normalizePort(val) {
